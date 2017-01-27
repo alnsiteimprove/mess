547ee8fe2f36ee88f9e9acb5947b6e4d97d717ad
Ola (Aleksandra) Nowak <aln@siteimprove.com>
2017-01-27 12:44:31 +0100

    Update event_tracking_examples.js

--- event_tracking_examples.js
+++ event_tracking_examples.js
@@ -1,5 +1,6 @@
+// log data to SI
 function log(c, a, l){
-  _sz.push(['event, c, a, l]);
+	_sz.push(['event, c, a, l]);
 }
 
 // tracking accordion elements:
@@ -12,21 +13,22 @@ function log(c, a, l){
 var height = jQuery(document).height() - jQuery(window).height();
 var scroll = 0;
 var third = height - height/4;
+            
 jQuery(window).scroll(function() {
-      if((scroll < height/4 && jQuery(window).scrollTop() > height/4) || (scroll > height/4 && jQuery(window).scrollTop() < height/4)){
-            scroll =  jQuery(window).scrollTop();          
-            log('Scroll', 'scroll', '25%');
-       }
-       if((scroll <  height/2 && jQuery(window).scrollTop() > height/2) || (scroll >  height/2 && jQuery(window).scrollTop() < height/2)){
-            scroll =  jQuery(window).scrollTop();  
-           	log('Scroll', 'scroll', '50%');
-       }
-       if((scroll < third && jQuery(window).scrollTop() > third) || (scroll > third && jQuery(window).scrollTop() < third)){
-            scroll   =  jQuery(window).scrollTop();          
-            log('Scroll', 'scroll', '75%');
-       }
-       if(jQuery(window).scrollTop() + jQuery(window).height() == jQuery(document).height()) {
-            scroll = jQuery(window).scrollTop();
-           	log('Scroll', 'scroll', '100%');
-       }
-  });
+	if((scroll < height/4 && jQuery(window).scrollTop() > height/4) || (scroll > height/4 && jQuery(window).scrollTop() < height/4)){
+		scroll =  jQuery(window).scrollTop();          
+		log('Scroll', 'scroll', '25%');
+	}
+	if((scroll <  height/2 && jQuery(window).scrollTop() > height/2) || (scroll >  height/2 && jQuery(window).scrollTop() < height/2)){
+		scroll =  jQuery(window).scrollTop();  
+		log('Scroll', 'scroll', '50%');
+	}
+	if((scroll < third && jQuery(window).scrollTop() > third) || (scroll > third && jQuery(window).scrollTop() < third)){
+		scroll   =  jQuery(window).scrollTop();          
+		log('Scroll', 'scroll', '75%');
+	}
+	if(jQuery(window).scrollTop() + jQuery(window).height() == jQuery(document).height()) {
+		scroll = jQuery(window).scrollTop();
+		log('Scroll', 'scroll', '100%');
+	}
+});
