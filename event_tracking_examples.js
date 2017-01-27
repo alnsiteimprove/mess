4a1825ebce2f5d75fe28789d4459d0ee08e490b0
Ola (Aleksandra) Nowak <aln@siteimprove.com>
2017-01-27 12:42:09 +0100

    Update event_tracking_examples.js

--- event_tracking_examples.js
+++ event_tracking_examples.js
@@ -8,26 +8,25 @@ function log(c, a, l){
 // tracking navigation buttons:
 
 
-// tracking scroll events:
-    // get page height
-    var body = document.body,html = document.documentElement;
-    var height = Math.max( body.scrollHeight, body.offsetHeight, 
-                       html.clientHeight, html.scrollHeight, html.offsetHeight );
-    var quarter = height/4;
-    var half = height/2;
-    var third = quarter + half;
-    
-    jQuery(window).scroll(function() {
-      if(jQuery(window).scrollTop()  == height/4){
-           log('Scroll', 'scroll', '25%');
+// tracking scroll events on 25%, 50%, 75% and 100% scrolled:
+var height = jQuery(document).height() - jQuery(window).height();
+var scroll = 0;
+var third = height - height/4;
+jQuery(window).scroll(function() {
+      if((scroll < height/4 && jQuery(window).scrollTop() > height/4) || (scroll > height/4 && jQuery(window).scrollTop() < height/4)){
+            scroll =  jQuery(window).scrollTop();          
+            log('Scroll', 'scroll', '25%');
        }
-       if(jQuery(window).scrollTop() == height/2){
-           log('Scroll', 'scroll', '50%');
+       if((scroll <  height/2 && jQuery(window).scrollTop() > height/2) || (scroll >  height/2 && jQuery(window).scrollTop() < height/2)){
+            scroll =  jQuery(window).scrollTop();  
+           	log('Scroll', 'scroll', '50%');
        }
-       if(jQuery(window).scrollTop() == (height - height/4)){
-           log('Scroll', 'scroll', '75%');
+       if((scroll < third && jQuery(window).scrollTop() > third) || (scroll > third && jQuery(window).scrollTop() < third)){
+            scroll   =  jQuery(window).scrollTop();          
+            log('Scroll', 'scroll', '75%');
        }
        if(jQuery(window).scrollTop() + jQuery(window).height() == jQuery(document).height()) {
-           log('Scroll', 'scroll', '100%');
+            scroll = jQuery(window).scrollTop();
+           	log('Scroll', 'scroll', '100%');
        }
   });
